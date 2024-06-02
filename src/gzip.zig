6cc404205fb853df2cc13679cda34e17fb98f825
Josh Hamill <joshua@transposit.com>
2024-06-02 19:33:01 -0400

    feat: parse out dynamic huffman code lengths

--- src/gzip.zig
+++ src/gzip.zig
@@ -30,9 +30,9 @@ pub fn inflate(buffer: *BitBuffer, writer: *Writer) !void {
     }
 }
 
-fn raw_inflate(buffer: *BitBuffer, writer: *Writer) !void {
+fn raw_inflate(_: *BitBuffer, writer: *Writer) !void {
     print("Writing raw data...", .{});
-    _ = try writer.write(buffer.data);
+    _ = try writer.write("Raw data isn't implemented, skipping...\n");
 }
 
 fn static_inflate(_: *BitBuffer, writer: *Writer) !void {
@@ -40,7 +40,30 @@ fn static_inflate(_: *BitBuffer, writer: *Writer) !void {
     _ = try writer.write("Static huffman isn't implemented, skipping...\n");
 }
 
-fn dynamic_inflate(_: *BitBuffer, writer: *Writer) !void {
-    print("Dynamic inflate...", .{});
+const HCLEN_ORDER = [_]usize{ 16, 17, 18, 0, 8, 7, 9, 6, 10, 5, 11, 4, 12, 3, 13, 2, 14, 1, 15 };
+
+fn dynamic_inflate(buffer: *BitBuffer, writer: *Writer) !void {
+    print("Dynamic inflate...\n", .{});
+
+    const hlit = buffer.get(5) + 257;
+    const hdist = buffer.get(5) + 1;
+    const hclen = buffer.get(4) + 4;
+
+    print("HLIT={d} HDIST={d} HCLEN={d}\n", .{ hlit, hdist, hclen });
+
+    var hclenLiterals: [19]u32 = undefined;
+    @memset(&hclenLiterals, 0);
+
+    for (0..hclen) |i| {
+        const len = buffer.get(3);
+        const literalIndex = HCLEN_ORDER[i];
+
+        hclenLiterals[literalIndex] = len;
+    }
+
+    for (hclenLiterals, 0..) |len, i| {
+        print("  {d} {d}\n", .{ i, len });
+    }
+
     _ = try writer.write("Dynamic huffman isn't implemented, skipping...\n");
 }
